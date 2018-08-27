6b811429eb66ddb52aa4d3ebaee9df0eeba93124
Igor Muzyka <igormuzyka42@gmail.com>
2018-08-27 16:54:22 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "RestorablePersistable",
+    products: [
+        .library(name: "RestorablePersistable", targets: ["RestorablePersistable"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/nvzqz/FileKit", from: "5.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "RestorablePersistable",
+            dependencies: ["FileKit"]),
+    ]
+)
